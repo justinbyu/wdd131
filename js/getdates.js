074871a2d9208dca589683aa21dd727f449a06ef
justinbyu <justinriverodiaz@gmail.com>
2026-05-09 16:54:30 +0800

    finish

---
+++ js/getdates.js
@@ -0,0 +1,7 @@
+// Current Year
+const currentYear = document.querySelector("#currentyear");
+currentYear.textContent = new Date().getFullYear();
+
+// Last Modified Date
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
